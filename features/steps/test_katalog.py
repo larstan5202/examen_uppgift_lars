2b8abeaaab6f11f804e3f3c9c49c4a4997e353a5
larstan5202 <lars.tangfelt@gmail.com>
2026-05-15 15:04:51 +0200

    Färdiga fungerade tester

--- features/steps/test_katalog.py
+++ features/steps/test_katalog.py
@@ -9,11 +9,11 @@ def page(browser):
     yield page
     page.close()
 
-@given("att användaren öppnar startsidan")
+@given("att användaren är på startsidan")
 def open_start(page):
     page.goto("https://tap-ht25-testverktyg.github.io/exam/", wait_until="networkidle")
     page.wait_for_selector('[data-testid="catalog"]')
 
 @then("visas bokkatalogen")
 def katalog_visible(page):
-    page.wait_for_selector('[data-testid="catalog-view"]')
+    page.wait_for_selector('[data-testid="catalog"]')
